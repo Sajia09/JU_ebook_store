62999bdf04259272575afa644ce2aec6d5931d07
tanzila <jucse28.343@gmail.com>
2024-02-08 21:29:25 +0600

    removeFromWishlist function creates in wishlistController

--- ebook-server/controllers/wishlistController.js
+++ ebook-server/controllers/wishlistController.js
@@ -7,9 +7,28 @@ class WishlistController {
   /**
    * Get wishlist of a user
    * @param {string} userId - User ID
-   * @returns {favourite<Array>} - Array of book ids in the wishlist
+   * @returns {Promise<Array>} - Array of book ids in the wishlist
    */
   async getWishlist(userId) {
     return await Wishlist.getWishlist(userId);
   }
+/**
+   * Add a book to the user's wishlist
+   * @param {string} userId - User ID
+   * @param {string} bookId - Book ID
+   * @returns {Promise<Wishlist>} - Updated wishlist
+   */
+async addToWishlist(userId, bookId) {
+    return await Wishlist.addToWishlist(userId, bookId);
+  }
+  /**
+   * Remove a book from the user's wishlist
+   * @param {string} userId - User ID
+   * @param {string} bookId - Book ID
+   * @returns {Promise<Wishlist>} - Updated wishlist
+   */
+  async removeFromWishlist(userId, bookId) {
+    return await Wishlist.removeFromWishlist(userId, bookId);
+  }
 }
+module.exports = WishlistController;
