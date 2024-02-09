5e53157b82b70ec9686909cd8109f7a9a2e57761
tanzila <jucse28.343@gmail.com>
2024-02-09 18:46:22 +0600

    new route allows fetching for a specific user by their user ID and defined a GET request

--- ebook-server/routes/wishlistRoutes.js
+++ ebook-server/routes/wishlistRoutes.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const WishlistController = require('../controllers/wishlistController');
+
+const wishlistController = new WishlistController();
+
+/**
+ * @route GET /api/wishlist/:userId
+ * @returns {Array} - Array of book ids in the wishlist
+ * @group Wishlist - Operations related to wishlist
+ */
+router.get('/:userId', async (req, res) => {
+  const { userId } = req.params;
+  const wishlist = await wishlistController.getWishlist(userId);
+  res.json(wishlist);
+});
