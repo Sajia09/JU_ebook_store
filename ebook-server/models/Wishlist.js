c80833c504f20a1e91fcc3dc4e5bbc82e9954002
tanzila <jucse28.343@gmail.com>
2024-02-12 23:30:18 +0600

    solve error in wishlist model

--- ebook-server/models/Wishlist.js
+++ ebook-server/models/Wishlist.js
@@ -1,5 +1,4 @@
-
-const { getDb } = require('../utils/database');
+const mongoose = require('mongoose');
 
 /**
  * @typedef Wishlist
@@ -7,53 +6,11 @@ const { getDb } = require('../utils/database');
  * @property {Array.<string>} books - Array of book ids added to the wishlist
  */
 
-/**
- * Retrieves the wishlist collection from the database.
- * @returns {Promise<Collection>} - Wishlist collection
- */
-const getWishlistCollection = async () => {
-  const db = await getDb();
-  return db.collection('wishlist');
-};
+const WishlistSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, required: true },
+  books: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Book' }]
+});
 
-module.exports = {
-  /**
-   * Get wishlist of a user
-   * @param {string} userId - User ID
-   * @returns {Promise<Array>} - Array of book ids in the wishlist
-   */
-  async getWishlist(userId) {
-    const wishlistCollection = await getWishlistCollection();
-    return wishlistCollection.findOne({ userId });
-  },
+const Wishlist = mongoose.model('Wishlist', WishlistSchema);
 
-  /**
-   * Add a book to the user's wishlist
-   * @param {string} userId - User ID
-   * @param {string} bookId - Book ID
-   * @returns {Promise<Wishlist>} - Updated wishlist
-   */
-  async addToWishlist(userId, bookId) {
-    const wishlistCollection = await getWishlistCollection();
-    return wishlistCollection.findOneAndUpdate(
-      { userId },
-      { $addToSet: { books: bookId } },
-      { returnOriginal: false, upsert: true }
-    );
-  },
-
-  /**
-   * Remove a book from the user's wishlist
-   * @param {string} userId - User ID
-   * @param {string} bookId - Book ID
-   * @returns {Promise<Wishlist>} - Updated wishlist
-   */
-  async removeFromWishlist(userId, bookId) {
-    const wishlistCollection = await getWishlistCollection();
-    return wishlistCollection.findOneAndUpdate(
-      { userId },
-      { $pull: { books: bookId } },
-      { returnOriginal: false }
-    );
-  }
-};
+module.exports = Wishlist;
