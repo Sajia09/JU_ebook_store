ec8e2adff5c8a9f8ffa86051fb9745b375b2af6f
Sajia09 <sajiaa09@gmail.com>
2024-02-26 00:24:35 +0600

    error resolved

--- ebook-server/controllers/bookController.js
+++ ebook-server/controllers/bookController.js
@@ -1,23 +1,23 @@
-/**
-* Controller for book-related operations.
- * @module controllers/bookController
- */
+import { getAllBooks as _getAllBooks, getBookById as _getBookById, createBook as _createBook } from '../models/BookModel';
 
-const Book = require('../models/Book');
-
-/**
- * Search for books by title.
- * @param {string} searchTerm - The search term to match against book titles.
- * @returns {Promise<Array>} - A promise that resolves to an array of books.
- */
-exports.searchBooks = async (searchTerm) => {
-  try {
-    const regex = new RegExp(searchTerm, 'i');
-    return await Book.find({ title: regex });
-  } 
-  catch (error) {
-    console.error('Error searching books:', error);
-    throw error;
+class BookController {
+  async getAllBooks(req, res) {
+    const books = await _getAllBooks();
+    res.json(books);
   }
-};
 
+  async getBookById(req, res) {
+    const { id } = req.params;
+    const book = await _getBookById(id);
+    res.json(book);
+  }
+
+  async createBook(req, res) {
+    const { title, author, description } = req.body;
+    const newBook = { title, author, description, reviews: [] };
+    const createdBook = await _createBook(newBook);
+    res.json(createdBook);
+  }
+}
+
+export default new BookController();
