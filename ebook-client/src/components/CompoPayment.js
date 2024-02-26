242077e389d8466a75f58bc36f1182455264b4ac
tanzila <jucse28.343@gmail.com>
2024-02-26 16:25:33 +0600

    add jsdoc in payment component file

--- ebook-client/src/components/CompoPayment.js
+++ ebook-client/src/components/CompoPayment.js
@@ -1,17 +1,29 @@
 import React, { useState } from 'react';
 import PaymentService from '../services/PaymentService';
 
+/**
+ * PaymentForm component handles the form submission for processing payments.
+ * It allows users to enter order ID and amount to initiate payment.
+ */
 const PaymentForm = () => {
     const [formData, setFormData] = useState({
         orderId: '',
         amount: '',
-        // Add other necessary fields
+       
     });
-
+  /**
+     * Handles changes in form inputs and updates the formData state accordingly.
+     * @param {Object} e - Event object representing the input change event.
+     */
     const handleChange = (e) => {
         const { name, value } = e.target;
         setFormData({ ...formData, [name]: value });
     };
+     /**
+     * Handles form submission for processing payment.
+     * Sends payment request to the backend and clears form data after successful payment.
+     * @param {Object} e - Event object representing the form submission event.
+     */
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
@@ -22,13 +34,14 @@ const PaymentForm = () => {
             setFormData({
                 orderId: '',
                 amount: '',
-                // Clear other fields as needed
+              
             });
         } catch (error) {
             console.error('Error processing payment:', error.message);
             // Handle error
         }
     };
+     // JSX for the PaymentForm component
     return (
         <div>
             <h2>Payment Form</h2>
